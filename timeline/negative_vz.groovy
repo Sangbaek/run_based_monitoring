0ee827db0f4c464e88c69a82a4ad3f6697459f88
Sangbaek <sangbaek@mit.edu>
2019-03-07 10:42:52 -0500

    negative

---
+++ timeline/negative_vz.groovy
@@ -0,0 +1,47 @@
+import org.jlab.groot.data.TDirectory
+import org.jlab.groot.data.GraphErrors
+import ROOTFitter
+
+def grtl = (1..6).collect{
+  def gr = new GraphErrors('sec'+it)
+  gr.setTitle("VZ (peak value) per sector")
+  gr.setTitleY("VZ (peak value) per sector")
+  gr.setTitleX("run number")
+  return gr
+}
+
+TDirectory out = new TDirectory()
+
+for(arg in args.drop(1)) {
+  TDirectory dir = new TDirectory()
+  dir.readFile(arg)
+
+  def name = arg.split('/')[-1]
+  def m = name =~ /\d\d\d\d/
+  def run = m[0].toInteger()
+
+  out.mkdir('/'+run)
+  out.cd('/'+run)
+
+  (0..<6).each{
+    // def h2 = dir.getObject('/elec/H_trig_vz_mom_S'+(it+1))
+    // def h1 = h2.projectionY()
+    def h1 = dir.getObject('/dc/H_dcm_vz_s'+(it+1))
+    h1.setName("sec"+(it+1))
+    h1.setTitle("VZ for negative")
+    h1.setTitleX("VZ for negative")
+
+    def f1 = ROOTFitter.fit(h1)
+
+    //grtl[it].addPoint(run, h1.getDataX(h1.getMaximumBin()), 0, 0)
+    grtl[it].addPoint(run, f1.getParameter(1), 0, 0)
+    out.addDataSet(h1)
+    out.addDataSet(f1)
+  }
+}
+
+
+out.mkdir('/timelines')
+out.cd('/timelines')
+grtl.each{ out.addDataSet(it) }
+out.writeFile('out_negative_VZ.hipo')
