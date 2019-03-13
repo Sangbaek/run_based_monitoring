5cfba1f81d960f1a83f5e78c6484e55c91d7c747
Sangbaek <sangbaek@mit.edu>
2019-03-12 20:54:10 -0400

    groovy scripts

--- groovy_codes/CND_time_all.groovy
+++ groovy_codes/CND_time_all.groovy
@@ -37,6 +37,7 @@ for(arg in args.drop(1)) {
     // def h1 = h2.projectionY()
     iL=it+1
     def h1 = dir.getObject(String.format("/cnd/H_CND_time_all%d",iL)
+    print h1.getMean()
     // h1.setName("all, layer"+iL)
     // h1.setTitle("CND time - start time")
     // h1.setTitleX("CND time - start time")
@@ -46,16 +47,16 @@ for(arg in args.drop(1)) {
     //grtl[it].addPoint(run, h1.getDataX(h1.getMaximumBin()), 0, 0)
     // grtl[it].addPoint(run, f1.getParameter(1), 0, 0)
     // grtl2[it].addPoint(run, f1.getParameter(2), 0, 0)
-    grtl[it].addPoint(run, h1.getMean(), 0, 0)
-    grtl2[it].addPoint(run, h1.getStdDev(), 0, 0)
-    out.addDataSet(h1)
+    // grtl[it].addPoint(run, h1.getMean(), 0, 0)
+    // grtl2[it].addPoint(run, h1.getStdDev(), 0, 0)
+    // out.addDataSet(h1)
     // out.addDataSet(f1)
   }
 }
 
 
-out.mkdir('/timelines')
-out.cd('/timelines')
-grtl.each{ out.addDataSet(it) }
-grtl2.each{ out.addDataSet(it) }
-out.writeFile('out_CND_time_all.hipo')
+// out.mkdir('/timelines')
+// out.cd('/timelines')
+// grtl.each{ out.addDataSet(it) }
+// grtl2.each{ out.addDataSet(it) }
+// out.writeFile('out_CND_time_all.hipo')
