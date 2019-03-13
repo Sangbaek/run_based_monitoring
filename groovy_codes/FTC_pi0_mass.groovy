24a5435e60d904a443af05312b74ff8782c674f5
Sangbaek <sangbaek@mit.edu>
2019-03-13 00:47:52 -0400

    groovy scripts

--- groovy_codes/FTC_pi0_mass.groovy
+++ groovy_codes/FTC_pi0_mass.groovy
@@ -3,16 +3,15 @@ import org.jlab.groot.data.GraphErrors
 // import ROOTFitter
 
   def grtl = new GraphErrors('FTC pi0 mass')
-  grtl.setTitle("FTC pi0 mass (peak value)")
-  grtl.setTitleY("FTC pi0 mass (peak value)")
+  grtl.setTitle("FTC time - start time (peak value)")
+  grtl.setTitleY("FTC time - start time (peak value)")
   grtl.setTitleX("run number")
-  return grtl
 
   def grtl2 = new GraphErrors('FTC pi0 mass, sigma')
-  grtl2.setTitle("FTC pi0 mass (sigma)")
-  grtl2.setTitleY("FTC pi0 mass (sigma)")
-  grtl2.setTitleX("run number")
-  return grtl2
+  grtl.setTitle("FTC time - start time (sigma)")
+  grtl.setTitleY("FTC time - start time (sigma)")
+  grtl.setTitleX("run number")
+
 
 TDirectory out = new TDirectory()
 for(arg in args.drop(1)) {
@@ -26,14 +25,15 @@ for(arg in args.drop(1)) {
   out.mkdir('/'+run)
   out.cd('/'+run)
 
-  def h1 = dir.getObject('/ft/hpi0sum')
+  def h1 = dir.getObject('/ft/hpi0mass')
+  h1.add(h2)
   // def f1 = ROOTFitter.fit(h1)
 
   //grtl[it].addPoint(run, h1.getDataX(h1.getMaximumBin()), 0, 0)
-  grtl.addPoint(run, h1.getMean(), 0, 0)
-  grtl2.addPoint(run, h1.getRMS(), 0, 0)
   // grtl.addPoint(run, f1.getParameter(1), 0, 0)
   // grtl2.addPoint(run, f1.getParameter(2), 0, 0)
+  grtl.addPoint(run, h1.getMean(), 0, 0)
+  grtl2.addPoint(run, h1.getRMS(), 0, 0)
   out.addDataSet(h1)
   // out.addDataSet(f1)
 
