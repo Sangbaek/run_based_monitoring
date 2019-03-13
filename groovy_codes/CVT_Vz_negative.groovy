0120a6ad23b0178261007d996795f90087a02157
Sangbaek <sangbaek@mit.edu>
2019-03-12 23:20:13 -0400

    groovy scripts

--- groovy_codes/CVT_Vz_negative.groovy
+++ groovy_codes/CVT_Vz_negative.groovy
@@ -26,7 +26,7 @@ for(arg in args) {
   //grtl[it].addPoint(run, h1.getDataX(h1.getMaximumBin()), 0, 0)
   // grtl.addPoint(run, f1.getParameter(1), 0, 0)
   grtl.addPoint(run, h1.getMean(), 0, 0)
-  out.addDataSet(h1)
+  // out.addDataSet(h1)
   // out.addDataSet(f1)
 
   out.mkdir('/'+run)
