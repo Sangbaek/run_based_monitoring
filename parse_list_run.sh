83ca0031dfec25270c4c1549e41006d670b9b2d8
Sangbaek <sangbaek@mit.edu>
2019-03-18 12:51:58 -0400

    runscript

--- parse_list_run.sh
+++ parse_list_run.sh
@@ -63,8 +63,8 @@ while IFS="	" read run_num Eb;do
 	fi
 	export groovy_input="$groovy_input ../plots$run_num/out_hiponame_$run_num.hipo"
 #done < $filename
-done < $listpath/list_run2.txt
-# done < $listpath/list_run.txt
+# done < $listpath/list_run2.txt
+done < $listpath/list_run.txt
 
 cd $pdir
 
