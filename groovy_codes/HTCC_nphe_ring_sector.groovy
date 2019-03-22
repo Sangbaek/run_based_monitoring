98a1a7161e6534ecca1a5593b1b9dce092ccd065
Sangbaek <sangbaek@mit.edu>
2019-03-22 00:15:57 -0400

    HTCC groovy

--- groovy_codes/HTCC_nphe_ring_sector.groovy
+++ groovy_codes/HTCC_nphe_ring_sector.groovy
@@ -31,7 +31,7 @@ for (s = 0; s <6; s++) {
     int counter = r + 4*s
     def h1 = dir.getObject(String.format('/HTCC/H_HTCC_nphe_s%d_r%d_side0',s+1,r+1)) //left
     def h2 = dir.getObject(String.format('/HTCC/H_HTCC_nphe_s%d_r%d_side1',s+1,r+1)) //right
-    h1.Add(h2)
+    h1.add(h2)
     h1.setName("sec"+(s+1) +"ring"+(r+1))
     h1.setTitle("HTCC Number of Photoelectrons")
     h1.setTitleX("HTCC Number of Photoelectrons")
