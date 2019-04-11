8fc3a8175f447b6722c0e8d117f322198eb67e02
Sangbaek <sangbaek@mit.edu>
2019-04-11 15:31:12 -0400

    implementing fitting function from Rafaella's code

--- groovy_codes/FTC_pi0_mass.groovy
+++ groovy_codes/FTC_pi0_mass.groovy
@@ -1,5 +1,10 @@
 import org.jlab.groot.data.TDirectory
 import org.jlab.groot.data.GraphErrors
+import org.jlab.groot.group.DataGroup;
+import org.jlab.groot.math.F1D;
+import org.jlab.groot.fitter.DataFitter;
+import org.jlab.groot.graphics.EmbeddedCanvas;
+
 // import ROOTFitter
 
   def grtl = new GraphErrors('Mean')
@@ -26,16 +31,32 @@ for(arg in args) {
   out.cd('/'+run)
 
   def h1 = dir.getObject('/ft/hpi0sum')
+  def fpi0 = new F1D("fpi0", "[amp]*gaus(x,[mean],[sigma])+[p0]+[p1]*x", 80 ,200 );
+  fpi0.setParameter(0, 0.0);
+  fpi0.setParameter(1, 140.0);
+  fpi0.setParameter(2, 2.0);
+  fpi0.setParameter(3, 0.0);
+  fpi0.setParameter(4, 0.0);
+  fpi0.setLineWidth(2);
+  fpi0.setOptStat("1111111");
+  double hAmp  = h1.getBinContent(h1.getMaximumBin());
+  double hMean = h1.getAxis().getBinCenter(h1.getMaximumBin());
+  double hRMS  = 10; //ns
+  fpi0.setParameter(0, hAmp);
+  fpi0.setParLimits(0, hAmp*0.8, hAmp*1.2);
+  fpi0.setParameter(1, hMean);
+  fpi0.setParLimits(1, hMean-hRMS, hMean+hRMS);
+  DataFitter.fit(fpi0,h1,"LQ");
+  h1.setFunction(null);
+
   // h1.add(h2)
   // def f1 = ROOTFitter.fit(h1)
 
   //grtl[it].addPoint(run, h1.getDataX(h1.getMaximumBin()), 0, 0)
-  // grtl.addPoint(run, f1.getParameter(1), 0, 0)
-  // grtl2.addPoint(run, f1.getParameter(2), 0, 0)
-  grtl.addPoint(run, h1.getMean(), 0, 0)
-  grtl2.addPoint(run, h1.getRMS(), 0, 0)
+  grtl.addPoint(run, fpi0.getParameter(1), 0, 0)
+  grtl2.addPoint(run, fpi0.getParameter(2), 0, 0)
   out.addDataSet(h1)
-  // out.addDataSet(f1)
+  out.addDataSet(fpi0)
 
 }
 
