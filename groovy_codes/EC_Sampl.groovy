1a90890ea38bbac7c57e8277325a09094cc82ca2
Sangbaek <sangbaek@mit.edu>
2019-04-11 20:32:56 -0400

    ec sampling fitting

--- groovy_codes/EC_Sampl.groovy
+++ groovy_codes/EC_Sampl.groovy
@@ -1,5 +1,10 @@
 import org.jlab.groot.data.TDirectory
 import org.jlab.groot.data.GraphErrors
+import org.jlab.groot.data.H1F
+import org.jlab.groot.group.DataGroup;
+import org.jlab.groot.math.F1D;
+import org.jlab.groot.fitter.DataFitter;
+import org.jlab.groot.graphics.EmbeddedCanvas;
 // import ROOTFitter
 
 def grtl = (1..6).collect{
@@ -30,7 +35,11 @@ for(arg in args) {
     h1.setTitle("ECAL Sampling Fraction")
     h1.setTitleX("ECAL Sampling Fraction")
 
-    // def f1 = ROOTFitter.fit(h1)
+    def f1 = new F1D("f1", "[amp]*gaus(x,[mean],[sigma])", 0.05, 0.2);
+    f1.setLineWidth(2);
+    f1.setOptStat("1111");
+    initTimeGaussFitPar(f1,h1);
+    DataFitter.fit(f1,h1,"LQ");
 
     // grtl[it].addPoint(run, h1.getDataX(h1.getMaximumBin()), 0, 0)
     // grtl[it].addPoint(run, f1.getParameter(1), 0, 0)
@@ -45,3 +54,19 @@ out.mkdir('/timelines')
 out.cd('/timelines')
 grtl.each{ out.addDataSet(it) }
 out.writeFile('ECAL_Sampling.hipo')
+
+private void initTimeGaussFitPar(F1D f1, H1F h1) {
+        double hAmp  = h1.getBinContent(h1.getMaximumBin());
+        double hMean = h1.getAxis().getBinCenter(h1.getMaximumBin());
+        double hRMS  = h1.getRMS(); //ns
+        // double rangeMin = (hMean - (3*hRMS));
+        // double rangeMax = (hMean + (3*hRMS));
+        // double pm = hRMS;
+        // f1.setRange(rangeMin, rangeMax);
+        f1.setParameter(0, hAmp);
+        // f1.setParLimits(0, hAmp*0.8, hAmp*1.2);
+        f1.setParameter(1, hMean);
+        // f1.setParLimits(1, hMean-pm, hMean+(pm));
+        f1.setParameter(2, 0.02);
+        // f1.setParLimits(2, 0.1*hRMS, 0.8*hRMS);
+}
