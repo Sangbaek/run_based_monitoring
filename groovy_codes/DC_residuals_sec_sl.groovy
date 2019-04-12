7d53bf4be20b22b96642db9108cb5febba63c421
Sangbaek <sangbaek@mit.edu>
2019-04-11 21:06:17 -0400

    DC residuals per sector

--- groovy_codes/DC_residuals_sec_sl.groovy
+++ groovy_codes/DC_residuals_sec_sl.groovy
@@ -99,8 +99,48 @@ for(arg in args) {
     h16.setTitle("DC residuals per sector per superlayer")
     h16.setTitleX("DC residuals per sector per superlayer (cm)")
 
-
     // def f1 = ROOTFitter.fit(h1)
+    def f11 = new F1D("f11", "[amp]*gaus(x,[mean],[sigma])", 0.15, 0.35);
+    f11.setName("fit sec"+sec_num"sl"+1)
+    f11.setLineWidth(2);
+    f11.setOptStat("1111");
+    initTimeGaussFitPar(f11,h11);
+    DataFitter.fit(f11,h11,"LQ");
+
+    def f12 = new F1D("f12", "[amp]*gaus(x,[mean],[sigma])", -0.2, 0.2);
+    f12.setName("fit sec"+sec_num"sl"+2)
+    f12.setLineWidth(2);
+    f12.setOptStat("1111");
+    initTimeGaussFitPar(f12,h12);
+    DataFitter.fit(f12,h12,"LQ");
+
+    def f13 = new F1D("f13", "[amp]*gaus(x,[mean],[sigma])", -0.2, 0.2);
+    f13.setName("fit sec"+sec_num"sl"+3)
+    f13.setLineWidth(3);
+    f13.setOptStat("1111");
+    initTimeGaussFitPar(f13,h13);
+    DataFitter.fit(f13,h13,"LQ");
+
+    def f14 = new F1D("f14", "[amp]*gaus(x,[mean],[sigma])", -0.2, 0.2);
+    f14.setName("fit sec"+sec_num"sl"+3)
+    f14.setLineWidth(3);
+    f14.setOptStat("1111");
+    initTimeGaussFitPar(f14,h14);
+    DataFitter.fit(f14,h14,"LQ");
+
+    def f15 = new F1D("f15", "[amp]*gaus(x,[mean],[sigma])", -0.2, 0.2);
+    f15.setName("fit sec"+sec_num"sl"+3)
+    f15.setLineWidth(3);
+    f15.setOptStat("1111");
+    initTimeGaussFitPar(f15,h15);
+    DataFitter.fit(f15,h15,"LQ");
+
+    def f16 = new F1D("f16", "[amp]*gaus(x,[mean],[sigma])", -0.2, 0.2);
+    f16.setName("fit sec"+sec_num"sl"+3)
+    f16.setLineWidth(3);
+    f16.setOptStat("1111");
+    initTimeGaussFitPar(f16,h16);
+    DataFitter.fit(f16,h16,"LQ");
 
     //grtl[it].addPoint(run, h1.getDataX(h1.getMaximumBin()), 0, 0)
     grtl1[it].addPoint(run, h11.getMean(), 0, 0)
@@ -129,3 +169,19 @@ grtl4.each{ out.addDataSet(it) }
 grtl5.each{ out.addDataSet(it) }
 grtl6.each{ out.addDataSet(it) }
 out.writeFile('DC_residuals_sec_sl.hipo')
+
+private void initTimeGaussFitPar(F1D f1, H1F h1) {
+        double hAmp  = h1.getBinContent(h1.getMaximumBin());
+        double hMean = h1.getAxis().getBinCenter(h1.getMaximumBin());
+        double hRMS  = h1.getRMS(); //ns
+        // double rangeMin = (hMean - (3*hRMS));
+        // double rangeMax = (hMean + (3*hRMS));
+        // double pm = hRMS;
+        // f1.setRange(rangeMin, rangeMax);
+        f1.setParameter(0, hAmp);
+        // f1.setParLimits(0, hAmp*0.8, hAmp*1.2);
+        f1.setParameter(1, hMean);
+        // f1.setParLimits(1, hMean-pm, hMean+(pm));
+        f1.setParameter(2, hRMS);
+        // f1.setParLimits(2, 0.1*hRMS, 0.8*hRMS);
+}
