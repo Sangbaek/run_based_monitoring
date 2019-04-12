4ae9600348605f63abdda4b6d216fca03013f356
Sangbaek <sangbaek@mit.edu>
2019-04-12 03:26:05 -0400

    FTOF fitting

--- groovy_codes/FTOF_pip_1b.groovy
+++ groovy_codes/FTOF_pip_1b.groovy
@@ -1,6 +1,10 @@
 import org.jlab.groot.data.TDirectory
 import org.jlab.groot.data.GraphErrors
-// import ROOTFitter
+import org.jlab.groot.data.H1F
+import org.jlab.groot.group.DataGroup;
+import org.jlab.groot.math.F1D;
+import org.jlab.groot.fitter.DataFitter;
+import org.jlab.groot.graphics.EmbeddedCanvas;
 
 def grtl = (1..6).collect{
   def gr = new GraphErrors('sec'+it)
@@ -31,12 +35,18 @@ for(arg in args) {
     h1.setTitleX("FTOF pip mass (GeV)")
 
     // def f1 = ROOTFitter.fit(h1)
+    def f1 = new F1D("f1", "[amp]*gaus(x,[mean],[sigma])",-0.2,0.2);
+    f1.setName("fit sec"+(it+1))
+    f1.setLineWidth(2);
+    f1.setOptStat("1111");
+    initTimeGaussFitPar(f1,h1);
+    DataFitter.fit(f1,h1,"LQ");
 
     //grtl[it].addPoint(run, h1.getDataX(h1.getMaximumBin()), 0, 0)
-    // grtl[it].addPoint(run, f1.getParameter(1), 0, 0)
-    grtl[it].addPoint(run, h1.getMean(), 0, 0)
+    grtl[it].addPoint(run, f1.getParameter(1), 0, 0)
+
     out.addDataSet(h1)
-    // out.addDataSet(f1)
+    out.addDataSet(f1)
   }
 }
 
@@ -45,3 +55,19 @@ out.mkdir('/timelines')
 out.cd('/timelines')
 grtl.each{ out.addDataSet(it) }
 out.writeFile('FTOF1b_pipm.hipo')
+
+private void initTimeGaussFitPar(F1D f1, H1F h1) {
+        double hAmp  = h1.getBinContent(h1.getMaximumBin());
+        double hMean = h1.getAxis().getBinCenter(h1.getMaximumBin());
+        double hRMS  = h1.getRMS(); //ns
+        double rangeMin = (hMean - (3*hRMS));
+        double rangeMax = (hMean + (3*hRMS));
+        // double pm = hRMS;
+        // f1.setRange(rangeMin, rangeMax);
+        f1.setParameter(0, hAmp);
+        // f1.setParLimits(0, hAmp*0.8, hAmp*1.2);
+        f1.setParameter(1, hMean);
+        // f1.setParLimits(1, hMean-pm, hMean+(pm));
+        f1.setParameter(2, hRMS);
+        // f1.setParLimits(2, 0.1*hRMS, 0.8*hRMS);
+}
