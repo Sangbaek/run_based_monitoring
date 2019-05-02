90a5a49bb66545f73baa6f87dd75742e38f10086
Sangbaek <sangbaek@mit.edu>
2019-05-02 19:06:06 -0400

    FTOF fit twice, mean and sigma separated

--- groovy_codes/FTOF_time_p1a_pion.groovy
+++ groovy_codes/FTOF_time_p1a_pion.groovy
@@ -8,13 +8,22 @@ import org.jlab.groot.graphics.EmbeddedCanvas;
 
 def grtl = (1..6).collect{
   def gr = new GraphErrors('sec'+it)
-  gr.setTitle("Mean FTOF time per sector, p1a pion")
-  gr.setTitleY("Mean FTOF time per sector, p1a pion (ns)")
+  gr.setTitle("FTOF time per sector, p1a pion, mean")
+  gr.setTitleY("FTOF time per sector, p1a pion, mean (ns)")
+  gr.setTitleX("run number")
+  return gr
+}
+
+def grtl = (1..6).collect{
+  def gr = new GraphErrors('sec'+it)
+  gr.setTitle("FTOF time per sector, p1a pion, sigma")
+  gr.setTitleY("FTOF time per sector, p1a pion, sigma (ns)")
   gr.setTitleX("run number")
   return gr
 }
 
 TDirectory out = new TDirectory()
+TDirectory out2 = new TDirectory()
 
 for(arg in args) {
   TDirectory dir = new TDirectory()
@@ -26,6 +35,8 @@ for(arg in args) {
 
   out.mkdir('/'+run)
   out.cd('/'+run)
+  out2.mkdir('/'+run)
+  out2.cd('/'+run)
 
   (0..<6).each{
     def h2 = dir.getObject('/FTOF/p1a_pad_vt_pion_S'+(it+1))
@@ -41,20 +52,25 @@ for(arg in args) {
     initTimeGaussFitPar(f1,h1);
     f1.setParameter(3,0);
     DataFitter.fit(f1,h1,"LQ");
-
-    //grtl[it].addPoint(run, h1.getDataX(h1.getMaximumBin()), 0, 0)
+    recursive_Gaussian_fitting(f1,h1)
     grtl[it].addPoint(run, f1.getParameter(1), 0, 0)
-    // grtl[it].addPoint(run, h1.getMean(), 0, 0)
+    grtl2[it].addPoint(run, f1.getParameter(2), 0, 0)
     out.addDataSet(h1)
     out.addDataSet(f1)
+    out2.addDataSet(h1)
+    out2.addDataSet(f1)
   }
 }
 
 
 out.mkdir('/timelines')
 out.cd('/timelines')
+out2.mkdir('/timelines')
+out2.cd('/timelines')
 grtl.each{ out.addDataSet(it) }
-out.writeFile('FTOF_time_p1a_pion.hipo')
+grtl2.each{ out2.addDataSet(it) }
+out.writeFile('FTOF_time_p1a_pion_mean.hipo')
+out2.writeFile('FTOF_time_p1a_pion_sigma.hipo')
 
 private void initTimeGaussFitPar(F1D f1, H1F h1) {
         double hAmp  = h1.getBinContent(h1.getMaximumBin());
@@ -70,4 +86,20 @@ private void initTimeGaussFitPar(F1D f1, H1F h1) {
         // f1.setParLimits(1, hMean-pm, hMean+(pm));
         f1.setParameter(2, hRMS);
         // f1.setParLimits(2, 0.1*hRMS, 0.8*hRMS);
+}
+
+private void recursive_Gaussian_fitting(F1D f1, H1F h1){
+        double rangeMin = f1.getParameter(1)-2*f1.getParameter(2)
+        double rangeMax = f1.getParameter(1)+2*f1.getParameter(2)
+        // limit fitting range as 2 sigma
+        f1.setRange(rangeMin, rangeMax)
+        // if with noise, don't fit such noise
+        if(f1.getNPars()>3){
+          (3..f1.getNPars()-1).each{
+            f1.setParLimits(it,f1.getParameter(it)*1.0, f1.getParameter(it)*1.0)
+          }
+        }
+        System.out.println(f1.getParameter(1))
+        DataFitter.fit(f1,h1,"LQ");
+        System.out.println(f1.getParameter(1))
 }
