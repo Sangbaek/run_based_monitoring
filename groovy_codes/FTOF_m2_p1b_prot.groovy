74e4ecd64316ca72e01876911d419859375a9d93
Sangbaek <sangbaek@mit.edu>
2019-05-02 20:18:40 -0400

    FTOF mass peak with sigma separated

--- groovy_codes/FTOF_m2_p1b_prot.groovy
+++ groovy_codes/FTOF_m2_p1b_prot.groovy
@@ -14,7 +14,15 @@ def grtl = (1..6).collect{
   return gr
 }
 
+def grtl2 = (1..6).collect{
+  def gr = new GraphErrors('sec'+it)
+  gr.setTitle("FTOF p1b proton mass^2 sigma")
+  gr.setTitleY("FTOF p1b proton mass^2 sigma (GeV^2)")
+  gr.setTitleX("run number")
+  return gr
+}
 TDirectory out = new TDirectory()
+TDirectory out2 = new TDirectory()
 
 for(arg in args) {
   TDirectory dir = new TDirectory()
@@ -26,6 +34,8 @@ for(arg in args) {
 
   out.mkdir('/'+run)
   out.cd('/'+run)
+  out2.mkdir('/'+run)
+  out2.cd('/'+run)
 
   (0..<6).each{
     def h2 = dir.getObject(String.format("/FTOF/H_FTOF_pos_mass_mom_pad1b_%d",it+1))
@@ -40,12 +50,14 @@ for(arg in args) {
     f1.setOptStat("1111");
     initTimeGaussFitPar(f1,h1);
     DataFitter.fit(f1,h1,"LQ");
-
+    recursive_Gaussian_fitting(f1,h1)
     //grtl[it].addPoint(run, h1.getDataX(h1.getMaximumBin()), 0, 0)
     grtl[it].addPoint(run, f1.getParameter(1), 0, f1.getParameter(2))
 
     out.addDataSet(h1)
     out.addDataSet(f1)
+    out2.addDataSet(h1)
+    out2.addDataSet(f1)
   }
 }
 
@@ -53,7 +65,11 @@ for(arg in args) {
 out.mkdir('/timelines')
 out.cd('/timelines')
 grtl.each{ out.addDataSet(it) }
-out.writeFile('FTOF_m2_p1b_prot.hipo')
+out.writeFile('FTOF_m2_p1b_prot_mean.hipo')
+out2.mkdir('/timelines')
+out2.cd('/timelines')
+grtl2.each{ out2.addDataSet(it) }
+out2.writeFile('FTOF_m2_p1b_prot_sigma.hipo')
 
 private void initTimeGaussFitPar(F1D f1, H1F h1) {
         double hAmp  = h1.getBinContent(h1.getMaximumBin());
@@ -69,4 +85,18 @@ private void initTimeGaussFitPar(F1D f1, H1F h1) {
         // f1.setParLimits(1, hMean-pm, hMean+(pm));
         f1.setParameter(2, 0.1);
         // f1.setParLimits(2, 0.1*hRMS, 0.8*hRMS);
+}
+
+private void recursive_Gaussian_fitting(F1D f1, H1F h1){
+        double rangeMin = f1.getParameter(1)-2*f1.getParameter(2)
+        double rangeMax = f1.getParameter(1)+2*f1.getParameter(2)
+        // limit fitting range as 2 sigma
+        f1.setRange(rangeMin, rangeMax)
+        // if with noise, don't fit such noise
+        if(f1.getNPars()>3){
+          (3..f1.getNPars()-1).each{
+            f1.setParLimits(it,f1.getParameter(it)*0.8, f1.getParameter(it)*1.2)
+          }
+        }
+        DataFitter.fit(f1,h1,"LQ");
 }
