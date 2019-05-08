fc7595fdb7f67233095af04fa3eb675512df5cf2
Sangbaek <sangbaek@mit.edu>
2019-05-07 21:07:04 -0400

    FTOF chi2 cuts larger

--- groovy_codes/FTOF_time.groovy
+++ groovy_codes/FTOF_time.groovy
@@ -108,7 +108,7 @@ private void recursive_Gaussian_fitting(F1D f1, H1F h1){
         }
         DataFitter.fit(f1,h1,"LQ");
         System.out.println("chi2 too large")
-        if (f1.getChiSquare()>500){
+        if (f1.getChiSquare()>1000){
           initTimeGaussFitPar(f1,h1);
           DataFitter.fit(f1,h1,"LQ");
         }
