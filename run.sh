28fde48d35d9ed19ddd3f99e98073980b45e380a
Sangbaek <sangbaek@mit.edu>
2019-05-13 15:50:41 -0400

    add updated runscripts for the latest monitoring

---
+++ run.sh
@@ -0,0 +1,95 @@
+#!/usr/bin/bash
+
+#Environment Initialization
+#for output coordinating
+export pdir=`pwd`
+
+#currently we use hipo3 for offline and timeline monitoring
+export coatjava='/group/clas12/packages/coatjava/5b.7.6/'
+export groovy=$coatjava"bin/run-groovy"
+
+#Where are monitoring codes by monitoring experts?
+export monplots='/work/clas12/rg-b/offline_monitoring/pass0/v2/plots*'
+#directory names
+#bmtbst central cnd ctof cvt dc ec forward ft ftof htcc ltcc rf trigger
+
+#hipo used for each detectors.
+#bmtbst - out_monitor
+#central - out_monitor
+#cnd - out_CND
+#ctof - out_CTOF
+#cvt - out_monitor
+#dc - out_TOF
+#ec - out_monitor
+#ft - out_FTOF
+#forward - out_monitor
+#rf - out_monitor
+#trigger - out_monitor
+#ftof - out_TOF, dst_mon
+#htcc - out_HTCC, out_monitor
+#ltcc - out_monitor, out_LTCC
+
+out_monitor=(bmtbst/bmt_Occupancy bmtbst/bmt_OnTrkLayers bmtbst/bst_Occupancy bst_OnTrkLayers \
+central/central_Km_num central/central_pim_num central/central_prot_num central/central_Kp_num central/central_pip_num \
+cvt/cvt_Vz_negative cvt/cvt_Vz_positive cvt/cvt_chi2_elec cvt/cvt_chi2_neg cvt/cvt_chi2_pos cvt/cvt_chi2norm \
+cvt/cvt_ndf cvt/cvt_p cvt/cvt_pathlen cvt/cvt_pt cvt/cvt_trks cvt/cvt_trks_neg cvt/cvt_trks_neg_rat cvt/cvt_trks_pos cvt/cvt_trks_pos_rat 
+trigger/rat_Km_num trigger/rat_neg_num trigger/rat_pos_numtrigger/rat_Kp_num \
+trigger/rat_neu_num trigger/rat_prot_num trigger/rat_elec_num trigger/rat_pim_num trigger/rat_muon_num trigger/rat_pip_num \
+forward/forward_Tracking_Elechi2 forward/forward_Tracking_EleVz forward/forward_Tracking_Poschi2 forward/forward_Tracking_PosVz \
+forward/forward_Tracking_Negchi2 forward/forward_Tracking_NegVz \
+ec/ec_Sampl ec/ec_gg_m ec/pip_time ec/pim_time \
+htcc/HTCC_nphe_sector \
+ltcc/LTCC_nphe_sector)
+#rf/rftime_diff rf/rftime_pim rf/rftime_elec rf/rftime_pip \
+out_TOF=(dc/dc_residuals_sec dc/dc_residuals_sec_sl dc/dc_tmax_sec_sl ftof/ftof_time)
+out_CTOF=(ctof/ctof_edep_pim ctof/ctof_m2_pim ctof/ctof_time_pim ctof/ctof_m2_pip ctof/ctof_time_positive ctof/ctof_time_negative)
+out_FT=(ft/ftc_pi0_mass ft/ftc_time_charged ft/ftc_time_neutral ft/fth_MIPS_energy ft/fth_MIPS_time ft/fth_MIPS_energy_board ft/fth_MIPS_time_board)
+dst_mon=(ftof/ftof_time_p1a_elec ftof/ftof_time_p1a_pion ftof/ftof_time_p1b_elec ftof/ftof_time_p1b_pion ftof/ftof_time_p2 \
+ftof/ftof_edep_p1a_elec ftof/ftof_edep_p1a_pion ftof/ftof_edep_p1b_elec ftof/ftof_edep_p1b_pion ftof/ftof_edep_p2 \
+ftof/ftof_m2_p1a_pim ftof/ftof_m2_p1a_pip ftof/ftof_m2_p1a_prot ftof/ftof_m2_p1b_pim ftof/ftof_m2_p1b_pip ftof/ftof_m2_p1b_prot)
+out_HTCC=(htcc/htcc_nphe_ring_sector)
+#out_LTCC=(ltcc/ltcc_had_nphe_sector)
+
+cd groovy_output
+
+for name in out_monitor out_TOF out_CTOF out_FT out_HTCC dst_mon
+do 
+	var=$name[@]
+	for script in ${!var}
+	do
+		# echo $script
+        $groovy ../groovy_codes/$script.groovy `find $monplots -name "$name*"`
+	done
+done
+
+out_dir=(bmtbst central cnd ctof cvt dc ec forward ft ftof htcc ltcc rf trigger)
+dir_array=out_dir[@]
+for dir in ${!dir_array}
+do
+	mkdir -p "$dir"
+done
+
+mv bmt_*.hipo bmtbst/
+mv bst_*.hipo bmtbst/
+mv cen_*.hipo central/
+mv cnd_*.hipo cnd/
+mv ctof_*.hipo ctof/
+mv cvt_*.hipo cvt/
+mv dc_*.hipo dc/
+mv ec_*.hipo ec/
+mv forward_*.hipo forward/
+mv ft_*.hipo ft/
+mv ftof_*.hipo ftof/
+mv htcc_*.hipo htcc/
+mv ltcc_*.hipo ltcc/
+mv rf_*.hipo rf/
+mv rat_* trigger/
+# for name in out_TOF
+# do
+#         var=$name[@]
+#         for script in ${!var}
+#         do
+#                 #$groovy ../groovy_codes/$script.groovy `find /volatile/clas12/rga/pass0/monitoring/plots* -name "$name*"` #rga
+#                 $groovy ../groovy_codes/$script.groovy `find /work/clas12/rg-b/offline_monitoring/plots* -name "$name*"` #rgb
+#         done
+# done
