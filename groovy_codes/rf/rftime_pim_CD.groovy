ff58ccc0fb2135e7a3aa61f00e96acb3694784dd
Andrey Kim <drewkenjo@users.noreply.github.com>
2019-07-31 15:12:39 -0400

    fitting RF

--- groovy_codes/rf/rftime_pim_CD.groovy
+++ groovy_codes/rf/rftime_pim_CD.groovy
@@ -7,12 +7,7 @@ import org.jlab.groot.fitter.DataFitter;
 import org.jlab.groot.graphics.EmbeddedCanvas;
 
 
-def grtl = new GraphErrors('RFtime_diff')
-grtl.setTitle("Average #pi^- rftime1, CD")
-grtl.setTitleY("Average #pi^- rftime1, CD (ns)")
-grtl.setTitleX("run number")
-
-TDirectory out = new TDirectory()
+def data = []
 
 for(arg in args) {
   TDirectory dir = new TDirectory()
@@ -23,16 +18,31 @@ for(arg in args) {
   def run = m[0].toInteger()
 
   def h1 = dir.getObject('/RF/H_pim_RFtime1')
+  def f1 = RFFitter.fit(h1)
 
-  grtl.addPoint(run, h1.getMean(), 0, 0)
-
-  out.mkdir('/'+run)
-  out.cd('/'+run)
-  out.addDataSet(h1)
+  data.add([run:run, mean:f1.getParameter(1), sigma:f1.getParameter(2), h1:h1, f1:f1])
 }
 
 
-out.mkdir('/timelines')
-out.cd('/timelines')
-grtl.each{ out.addDataSet(it) }
-out.writeFile('rftime_pim_CD.hipo')
+['mean', 'sigma'].each{name ->
+  TDirectory out = new TDirectory()
+
+  def grtl = new GraphErrors('RFtime_pim_CD'+name)
+  grtl.setTitle("Average #pi<sup>-</sup> rftime1, CD ("+name+")")
+  grtl.setTitleY("Average #pi<sup>-</sup> rftime1, CD (ns)")
+  grtl.setTitleX("run number")
+
+  data.each{
+    grtl.addPoint(it.run, it[name], 0, 0)
+    out.mkdir('/'+it.run)
+    out.cd('/'+it.run)
+    out.addDataSet(it.h1)
+    out.addDataSet(it.f1)
+  }
+
+  out.mkdir('/timelines')
+  out.cd('/timelines')
+  out.addDataSet(grtl)
+  out.writeFile('rftime_pim_CD_'+name+'.hipo')
+}
+
