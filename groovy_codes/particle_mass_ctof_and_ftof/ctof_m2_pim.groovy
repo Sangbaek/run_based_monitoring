f8bd97f6babd4e0c80da2c895f2a30a1085e2f0e
Sangbaek <sangbaek@mit.edu>
2019-10-18 17:36:56 -0400

    separate fitting as lib file, and test it on pim only

--- groovy_codes/particle_mass_ctof_and_ftof/ctof_m2_pim.groovy
+++ groovy_codes/particle_mass_ctof_and_ftof/ctof_m2_pim.groovy
@@ -6,18 +6,41 @@ import org.jlab.groot.math.F1D;
 import org.jlab.groot.fitter.DataFitter;
 import org.jlab.groot.graphics.EmbeddedCanvas;
 
-def grtl = new GraphErrors('mass2')
-grtl.setTitle("CTOF mass^2 peak, #pi^-")
-grtl.setTitleY("CTOF mass^2 peak, #pi^- (GeV^2)")
-grtl.setTitleX("run number")
+for(arg in args) {
+  TDirectory dir = new TDirectory()
+  dir.readFile(arg)
 
-def grtl2 = new GraphErrors('sigma')
-grtl2.setTitle("CTOF mass^2 sigma, #pi^-")
-grtl2.setTitleY("CTOF mass^2 sigma, #pi^- (GeV^2)")
-grtl2.setTitleX("run number")
+  def name = arg.split('/')[-1]
+  def m = name =~ /\d\d\d\d/
+  def run = m[0].toInteger()
 
-TDirectory out = new TDirectory()
-TDirectory out2 = new TDirectory()
+  def h1 = dir.getObject('/ctof/H_CTOF_neg_mass')
+  def f1 = CTOFFitter.fit(h1)
+
+  data.add([run:run, peak:f1.getParameter(1), sigma:f1.getParameter(2).abs(), h1:h1, f1:f1])
+}
+
+['peak', 'sigma'].each{name ->
+  TDirectory out = new TDirectory()
+
+  def grtl = new GraphErrors(name)
+  grtl.setTitle("CTOF mass^2 "+name+", #pi^-")
+  grtl.setTitleY("CTOF mass^2 "+name+", #pi^- (GeV^2)")
+  grtl.setTitleX("run number")
+
+  data.each{
+    grtl.addPoint(it.run, it[name], 0, 0)
+    out.mkdir('/'+it.run)
+    out.cd('/'+it.run)
+    out.addDataSet(it.h1)
+    out.addDataSet(it.f1)
+  }
+
+  out.mkdir('/timelines')
+  out.cd('/timelines')
+  out.addDataSet(grtl)
+  out.writeFile('rftime_electron_CD_'+name+'.hipo')
+}
 
 for(arg in args) {
   TDirectory dir = new TDirectory()
@@ -26,7 +49,7 @@ for(arg in args) {
   def name = arg.split('/')[-1]
   def m = name =~ /\d\d\d\d/
   def run = m[0].toInteger()
-  def h1 = dir.getObject('/ctof/H_CTOF_neg_mass')
+  def h1 = dir.getObject(')
 
   // def f1 = ROOTFitter.fit(h1)
   def f1 = new F1D("fit:"+h1.getName(), "[amp]*gaus(x,[mean],[sigma])",-0.2,0.2);
