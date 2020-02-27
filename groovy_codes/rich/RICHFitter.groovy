12452b59c69ce4ab0d3481fe3685e40177d34891
Sangbaek <sangbaek@mit.edu>
2020-02-26 22:25:04 -0500

    update fitting ftns and params

--- groovy_codes/rich/RICHFitter.groovy
+++ groovy_codes/rich/RICHFitter.groovy
@@ -5,14 +5,14 @@ import org.jlab.groot.math.F1D
 
 class RICHFitter {
   static F1D timefit(H1F h1) {
-    def f1 = new F1D("fit:"+h1.getName(), "[amp]*gaus(x,[mean],[sigma])+[p0]", -1.0, 1.0)
+    def f1 = new F1D("fit:"+h1.getName(), "[amp]*gaus(x,[mean],[sigma])+[p0]+[p1]*x", -1.0, 1.0)
     double hMean = h1.getAxis().getBinCenter(h1.getMaximumBin())
-    double hRMS  = 5
+    double hRMS  = 0.5
 
     f1.setParameter(1, hMean)
     f1.setParLimits(1, hMean-2*hRMS, hMean+2*hRMS)
     f1.setParameter(2, hRMS)
-    f1.setParLimits(2, 0, 20)
+    f1.setParLimits(2, 0, 5)
     f1.setParameter(3, 0)
     f1.setParLimits(3, 0, h1.getMax()*0.5)
     f1.setRange(hMean-25,hMean+25)
@@ -20,7 +20,7 @@ class RICHFitter {
     def makefit = {func->
       hMean = func.getParameter(1)
       hRMS = func.getParameter(2).abs()
-      func.setRange(hMean-2.5*hRMS,hMean+2.5*hRMS)
+      func.setRange(hMean-5*hRMS,hMean+5*hRMS)
       DataFitter.fit(func,h1,"Q")
       return [func.getChiSquare(), (0..<func.getNPars()).collect{func.getParameter(it)}]
     }
