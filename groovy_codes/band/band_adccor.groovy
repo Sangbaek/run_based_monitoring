19ec2a35af7a6961dfc7939933a316f2a32ee02d
Sangbaek <sangbaek@mit.edu>
2020-04-14 16:26:51 -0400

    change style from direct plotting to saving into dict and draw: BAND

--- groovy_codes/band/band_adccor.groovy
+++ groovy_codes/band/band_adccor.groovy
@@ -1,17 +1,10 @@
 import org.jlab.groot.data.TDirectory
 import org.jlab.groot.data.GraphErrors
-// import ROOTFitter
 
-def grtl = (1..2).collect{
-  def gr = new GraphErrors('Comb'+it)
-  gr.setTitle("BAND adc LR Sector Combination")
-  gr.setTitleY("Mean Value of sqrt( adcLcorr * adcRcorr )")
-  gr.setTitleX("run number")
-  return gr
-}
-
+def data = []
 
 TDirectory out = new TDirectory()
+out.mkdir('/timelines')
 
 for(arg in args) {
   TDirectory dir = new TDirectory()
@@ -21,22 +14,26 @@ for(arg in args) {
   def m = name =~ /\d{4,5}/
   def run = m[0].toInteger()
 
-    // def h2 = dir.getObject('/elec/H_trig_vz_mom_S'+(it+1))
-    // def h1 = h2.projectionY()
-    def h1 = dir.getObject('/BAND/H_BAND_ADC_LR_SectorCombination1')
-    def h2 = dir.getObject('/BAND/H_BAND_ADC_LR_SectorCombination2')
+  def h1 = dir.getObject('/BAND/H_BAND_ADC_LR_SectorCombination1')
+  def h2 = dir.getObject('/BAND/H_BAND_ADC_LR_SectorCombination2')
 
-    grtl[0].addPoint(run, h1.getMean(), 0, 0)
-    grtl[1].addPoint(run, h2.getMean(), 0, 0)
-    // grtl[it].addPoint(run, f1.getParameter(1), 0, 0)
-    out.mkdir('/'+run)
-    out.cd('/'+run)
-    out.addDataSet(h1)
-    out.addDataSet(h2)
+  data.add([run:run, Comb1:h1, Comb2:h2])
+  out.mkdir('/'+run)
 }
 
+["Comb1", "Comb2"].each{ name ->
+  def gr = new GraphErrors(name)
+  gr.setTitle("BAND adc LR Sector Combination")
+  gr.setTitleY("Mean Value of sqrt( adcLcorr * adcRcorr )")
+  gr.setTitleX("run number")
+  
+  data.each{
+    out.cd('/'+it.run)
+    out.addDataSet(it[name])
+    gr.addPoint(it.run, it[name].getMean(), 0, 0)
+  }
+  out.cd('/timelines')
+  out.addDataSet(gr)
+}
 
-out.mkdir('/timelines')
-out.cd('/timelines')
-grtl.each{ out.addDataSet(it) }
 out.writeFile('band_adcCorr.hipo')
--- groovy_codes/band/band_meantimetdc.groovy
+++ groovy_codes/band/band_meantimetdc.groovy
@@ -1,16 +1,10 @@
 import org.jlab.groot.data.TDirectory
 import org.jlab.groot.data.GraphErrors
-// import ROOTFitter
 
-def grtl = (1..2).collect{
-  def gr = new GraphErrors('Comb'+it)
-  gr.setTitle("BAND mean time tdc sector combination")
-  gr.setTitleY("maximum location of meantimeTDC – sqrt(x^2+y^2+z^2)/c (ns)")
-  gr.setTitleX("run number")
-  return gr
-}
+def data = []
 
 TDirectory out = new TDirectory()
+out.mkdir('/timelines')
 
 for(arg in args) {
   TDirectory dir = new TDirectory()
@@ -20,22 +14,26 @@ for(arg in args) {
   def m = name =~ /\d{4,5}/
   def run = m[0].toInteger()
 
-    // def h2 = dir.getObject('/elec/H_trig_vz_mom_S'+(it+1))
-    // def h1 = h2.projectionY()
-    def h1 = dir.getObject('/BAND/H_BAND_MeanTimeTDC_SectorCombination1')
-    def h2 = dir.getObject('/BAND/H_BAND_MeanTimeTDC_SectorCombination2')
+  def h1 = dir.getObject('/BAND/H_BAND_MeanTimeTDC_SectorCombination1')
+  def h2 = dir.getObject('/BAND/H_BAND_MeanTimeTDC_SectorCombination2')
 
-    grtl[0].addPoint(run, h1.getAxis().getBinCenter(h1.getMaximumBin()), 0, 0)
-    grtl[1].addPoint(run, h2.getAxis().getBinCenter(h2.getMaximumBin()), 0, 0)
-    // grtl[it].addPoint(run, f1.getParameter(1), 0, 0)
-    out.mkdir('/'+run)
-    out.cd('/'+run)
-    out.addDataSet(h1)
-    out.addDataSet(h2)
+  data.add([run:run, Comb1:h1, Comb2:h2])
+  out.mkdir('/'+run)
 }
 
+["Comb1", "Comb2"].each{ name ->
+  def gr = new GraphErrors(name)
+  gr.setTitle("BAND mean time tdc sector combination")
+  gr.setTitleY("maximum location of meantimeTDC – sqrt(x^2+y^2+z^2)/c (ns)")
+  gr.setTitleX("run number")
+  
+  data.each{
+    out.cd('/'+it.run)
+    out.addDataSet(it[name])
+    gr.addPoint(it.run, it[name].getAxis().getBinCenter(h1.getMaximumBin()), 0, 0)
+  }
+  out.cd('/timelines')
+  out.addDataSet(gr)
+}
 
-out.mkdir('/timelines')
-out.cd('/timelines')
-grtl.each{ out.addDataSet(it) }
 out.writeFile('band_meantimetdc.hipo')
