f20a13946cae3ec8c4df723b4f092fd2e2cde9e3
Sangbaek <sangbaek@mit.edu>
2020-04-15 18:17:14 -0400

    change style from direct plotting to saving into dict and draw: ltcc

--- groovy_codes/ltcc/ltcc_nphe_sector.groovy
+++ groovy_codes/ltcc/ltcc_nphe_sector.groovy
@@ -1,17 +1,7 @@
 import org.jlab.groot.data.TDirectory
 import org.jlab.groot.data.GraphErrors
-// import ROOTFitter
 
-def grtl = (0..1).collect{
-  sec_num=2*it+3
-  def gr = new GraphErrors('sec'+sec_num)
-  gr.setTitle("LTCC Number of Photoelectrons for electrons")
-  gr.setTitleY("LTCC Number of Photoelectrons for electrons per sector")
-  gr.setTitleX("run number")
-  return gr
-}
-
-TDirectory out = new TDirectory()
+data = []
 
 for(arg in args) {
   TDirectory dir = new TDirectory()
@@ -21,29 +11,37 @@ for(arg in args) {
   def m = name =~ /\d{4,5}/
   def run = m[0].toInteger()
 
-  out.mkdir('/'+run)
-  out.cd('/'+run)
-
-  (0..1).each{
-    sec_num=2*it+3
-    def h2 = dir.getObject('/elec/H_trig_LTCCn_theta_S'+(sec_num))
-    def h1 = h2.projectionY()
-    h1.setName("sec"+(sec_num))
-    h1.setTitle("LTCC Number of Photoelectrons for elec")
-    h1.setTitleX("LTCC Number of Photoelectrons for elec")
-
-    // def f1 = ROOTFitter.fit(h1)
-
-    // grtl[it].addPoint(run, h1.getDataX(h1.getMaximumBin()), 0, 0)
-    // grtl[it].addPoint(run, f1.getParameter(1), 0, 0)
-    grtl[it].addPoint(run, h1.getMean(), 0, 0)
-    out.addDataSet(h1)
-    // out.addDataSet(f1)
+  def hlist = [3,5].collect{
+    def hist = dir.getObject('/elec/H_trig_LTCCn_theta_S'+it).projectionY()
+    hist.setName("sec"+(it))
+    hist.setTitle("LTCC Number of Photoelectrons from electrons")
+    hist.setTitleX("LTCC Number of Photoelectrons from electrons")
+    hist
   }
+  data.add([run:run, h3:hlist[0], h5:hlist[1]])
 }
 
 
+TDirectory out = new TDirectory()
 out.mkdir('/timelines')
-out.cd('/timelines')
-grtl.each{ out.addDataSet(it) }
+
+[3,5].each{ sec->
+  def grtl = new GraphErrors('sec'+sec)
+  grtl.setTitle("LTCC Number of Photoelectrons from electrons")
+  grtl.setTitleY("LTCC Number of Photoelectrons from electrons per sector")
+  grtl.setTitleX("run number")
+
+  data.each{
+    if (sec==3){
+      out.mkdir('/'+it.run)
+    }
+    out.cd('/'+it.run)
+
+    out.addDataSet(it["h"+sec])
+    grtl.addPoint(it.run, it["h"+sec].getMean(), 0, 0)
+  }
+  out.cd('/timelines')
+  out.addDataSet(grtl)
+}
+
 out.writeFile('ltcc_elec_nphe_sec.hipo')
