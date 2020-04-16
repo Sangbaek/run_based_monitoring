df54886aeb008c2edfe7c316bde1f0e139ab0517
Sangbaek <sangbaek@mit.edu>
2020-04-16 18:26:07 -0400

    sampling fraction fitting works better with offset

--- groovy_codes/fitter/ECFitter.groovy
+++ groovy_codes/fitter/ECFitter.groovy
@@ -69,21 +69,28 @@ class ECFitter {
     f1.setParameter(1, hMean);
     f1.setParameter(2, hRMS);
 
-    def makefit = {func->
-      hMean = func.getParameter(1)
+    def makefits = {func->
       hRMS = func.getParameter(2).abs()
-      func.setRange(hMean-2.5*hRMS,hMean+2.5*hRMS)
+      func.setRange(hMean-2.5*hRMS, hMean+2.5*hRMS)
       DataFitter.fit(func,h1,"Q")
       return [func.getChiSquare(), (0..<func.getNPars()).collect{func.getParameter(it)}]
     }
+    def fits1 = (0..10).collect{makefits(f1)}
 
-    def fits1 = (0..20).collect{makefit(f1)}
-    def bestfit = fits1.sort()[0]
-    f1.setParameters(*bestfit[1])
-    //makefit(f1)
+    def f2 = new F1D("fit:"+h1.getName(), "[amp]*gaus(x,[mean],[sigma])+[const]+[slope]*x",-0.2,0.2);
 
-    return f1
+    fits1.sort()[0][1].eachWithIndex{par,ipar->
+      f2.setParameter(ipar, par)
+    }
+
+    def fits2 = (0..10).collect{makefits(f2)}
+
+    def bestfit = fits2.sort()[0]
+    f2.setParameters(*bestfit[1])
+
+    return f2
   }
+
 
 
 }
