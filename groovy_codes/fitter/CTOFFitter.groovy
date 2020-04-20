00cfd4eb5bb67d4c0ed157b50a9f986df077d3d7
Sangbaek <sangbaek@mit.edu>
2020-04-19 23:03:41 -0400

    ctof time and tdcadc time diff fitting ftns updated

--- groovy_codes/fitter/CTOFFitter.groovy
+++ groovy_codes/fitter/CTOFFitter.groovy
@@ -10,7 +10,7 @@ class CTOFFitter {
     def f1 = new F1D("fit:"+h1.getName(), "[amp]*gaus(x,[mean],[sigma])", -5.0, 5.0)
     double hAmp  = h1.getBinContent(h1.getMaximumBin());
     double hMean = h1.getAxis().getBinCenter(h1.getMaximumBin())
-    double hRMS  = h1.getRMS();
+    double hRMS  = Math.min(h1.getRMS(), 0.2)
     double rangeMin = (hMean - 2.5*hRMS);
     double rangeMax = (hMean + 2.5*hRMS);
     f1.setRange(rangeMin, rangeMax);
@@ -68,6 +68,74 @@ class CTOFFitter {
     //makefit(f1)
     return f1
   }
+
+
+  static F1D tdcadcdifffit_right(H1F h1) {
+    def f1 = new F1D("fit:"+h1.getName(), "[amp]*gaus(x,[mean],[sigma])", -5.0, 5.0)
+    def peakbin = (1000..1500).max{h1.getBinContent(it)}
+    double hAmp  = h1.getBinContent(peakbin);
+    double hMean = h1.getAxis().getBinCenter(peakbin)
+    double hRMS  = h1.getRMS();
+    double factor1 = 3.0
+    double factor2 = 1.57
+    double rangeMin = (hMean - factor1*0.5);
+    double rangeMax = (hMean + factor2*0.5);
+    f1.setRange(rangeMin, rangeMax);
+    f1.setParameter(0, hAmp);
+    // f1.setParLimits(0, hAmp*0.8, hAmp*1.2);
+    f1.setParameter(1, hMean);
+    // f1.setParLimits(1, 0, 0.5);
+    f1.setParameter(2, 1);
+    // f1.setParLimits(2, 0.5*hRMS, 1.5*hRMS);
+
+    def makefit = {func->
+      hMean = func.getParameter(1)
+      hRMS = func.getParameter(2).abs()
+      func.setRange(hMean-2*hRMS,hMean+2*hRMS)
+      DataFitter.fit(func,h1,"Q")
+      return [func.getChiSquare(), (0..<func.getNPars()).collect{func.getParameter(it)}]
+    }
+
+    def fits1 = (0..20).collect{makefit(f1)}
+    def bestfit = fits1.sort()[0]
+    f1.setParameters(*bestfit[1])
+    //makefit(f1)
+    return f1
+  }
+
+  static F1D tdcadcdifffit_left(H1F h1) {
+    def f1 = new F1D("fit:"+h1.getName(), "[amp]*gaus(x,[mean],[sigma])", -5.0, 5.0)
+    def peakbin = (400..900).max{h1.getBinContent(it)}
+    double hAmp  = h1.getBinContent(peakbin);
+    double hMean = h1.getAxis().getBinCenter(peakbin)
+    double hRMS  = h1.getRMS();
+    double factor1 = 3.0
+    double factor2 = 1.57
+    double rangeMin = (hMean - factor1*0.5);
+    double rangeMax = (hMean + factor2*0.5);
+    f1.setRange(rangeMin, rangeMax);
+    f1.setParameter(0, hAmp);
+    // f1.setParLimits(0, hAmp*0.8, hAmp*1.2);
+    f1.setParameter(1, hMean);
+    // f1.setParLimits(1, 0, 0.5);
+    f1.setParameter(2, 1);
+    // f1.setParLimits(2, 0.5*hRMS, 1.5*hRMS);
+
+    def makefit = {func->
+      hMean = func.getParameter(1)
+      hRMS = func.getParameter(2).abs()
+      func.setRange(hMean-2*hRMS,hMean+2*hRMS)
+      DataFitter.fit(func,h1,"Q")
+      return [func.getChiSquare(), (0..<func.getNPars()).collect{func.getParameter(it)}]
+    }
+
+    def fits1 = (0..20).collect{makefit(f1)}
+    def bestfit = fits1.sort()[0]
+    f1.setParameters(*bestfit[1])
+    //makefit(f1)
+    return f1
+  }
+
 
   static edepfit(H1F h1){
     def f1 = new F1D("fit:"+h1.getName(),"[amp]*landau(x,[mean],[sigma])+[p0]*exp(-[p1]*x)", 0, 30.0);
