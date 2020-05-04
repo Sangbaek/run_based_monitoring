1f2712f24eccfd92b55a2641f31a35fc0f1be90f
Sangbaek <sangbaek@mit.edu>
2020-05-04 13:35:39 -0400

    narrowing down fitting range for better quality

--- groovy_codes/fitter/FTFitter.groovy
+++ groovy_codes/fitter/FTFitter.groovy
@@ -52,7 +52,7 @@ class FTFitter {
     def makefit = {func->
       hMean = func.getParameter(1)
       hRMS = func.getParameter(2).abs()
-      func.setRange(hMean-2.5*hRMS,hMean+2.5*hRMS)
+      func.setRange(hMean-2*hRMS,hMean+2*hRMS)
       DataFitter.fit(func,h1,"Q")
       return [func.getChiSquare(), (0..<func.getNPars()).collect{func.getParameter(it)}]
     }
