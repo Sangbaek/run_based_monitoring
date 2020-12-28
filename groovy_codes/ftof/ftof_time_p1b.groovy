6d350b665bd3232b634c17a90c613d46fed87c85
Sangbaek <sangbaek@mit.edu>
2020-12-29 01:24:07 +0900

    try add point with vertical errorbar at graph

--- groovy_codes/ftof/ftof_time_p1b.groovy
+++ groovy_codes/ftof/ftof_time_p1b.groovy
@@ -13,6 +13,7 @@ def processDirectory(dir, run) {
   def meanlist = []
   def sigmalist = []
   def chi2list = []
+  def meanerrorlist = []
   def histlist =   (0..<6).collect{
     def h1 = dir.getObject('/tof/p1b_dt_S'+(it+1))
     def f1 = FTOFFitter.timefit_p1b(h1)
@@ -21,9 +22,11 @@ def processDirectory(dir, run) {
     meanlist.add(f1.getParameter(1))
     sigmalist.add(f1.getParameter(2).abs())
     chi2list.add(f1.getChiSquare())
+    meanerrorlist.add(f1.getParError(1))
+    sigmaerrorlist.add(f1.getParError(2))
     return h1
   }
-  data[run] = [run:run, hlist:histlist, flist:funclist, mean:meanlist, sigma:sigmalist, clist:chi2list]
+  data[run] = [run:run, hlist:histlist, flist:funclist, mean:meanlist, sigma:sigmalist, clist:chi2list, meanerrorlist:meanerrorlist, sigmaerrorlist:sigmaerrorlist]
 }
 
 
@@ -46,7 +49,7 @@ def close() {
         out.cd('/'+it.run)
         out.addDataSet(it.hlist[sec])
         out.addDataSet(it.flist[sec])
-        grtl.addPoint(it.run, it[name][sec], 0, 0)
+        grtl.addPoint(it.run, it[name][sec], 0, it[name+'errorlist'][sec])
       }
       out.cd('/timelines')
       out.addDataSet(grtl)
